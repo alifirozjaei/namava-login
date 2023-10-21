9e19da5bf508c0698eb47fdecc267c84263b0d9b
Ali L Firozjaeai <ali.firozjaeii@gmail.com>
2023-10-21 21:06:32 +0330

    get input value and save it into state & create validation function

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./input.module.css";
-const Input = ({ id, type, placeholder, label }) => {
+const Input = ({ id, type, placeholder, label, changeHandler, value }) => {
   return (
     <div className={styles["form-control"]}>
       <label htmlFor="passwrod" className={styles["form-label"]}>
@@ -11,6 +11,8 @@ const Input = ({ id, type, placeholder, label }) => {
         id={id}
         className={styles["form-input"]}
         placeholder={placeholder}
+        onChange={(e) => changeHandler(e.target.value)}
+        value={value}
       />
     </div>
   );
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../Form/Input.jsx";
 import styles from "./loginform.module.css";
+import validateEmail from "../../utils/validateEmail.js";
+import validatePassword from "../../utils/validatePassword.js";
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeEmailHandler = (emailValue) => {
+    setEmail(emailValue.trim());
+  };
+
+  const changePasswordHandler = (passwordValue) => {
+    setPassword(passwordValue.trim());
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(
+      email,
+      password,
+      validateEmail(email),
+      validatePassword(password)
+    );
+  };
+
   return (
-    <form className={styles["form"]}>
+    <form className={styles["form"]} onSubmit={submitHandler}>
       <h3>ورود از طریق ایمیل</h3>
       <p>لطفا ایمیل و رمز عبور خود را وارد کنید</p>
 
-      <Input id="email" type="email" placeholder="ایمیل" label="ایمیل" />
+      <Input
+        id="email"
+        type="text"
+        placeholder="ایمیل"
+        label="ایمیل"
+        changeHandler={changeEmailHandler}
+        value={email}
+      />
 
       <Input
         id="password"
         type="password"
         placeholder="رمز عبور"
         label="رمز عبور"
+        changeHandler={changePasswordHandler}
+        value={password}
       />
 
-      <button className={styles["form-btn"]} type="submit" disabled>
+      <button
+        className={styles["form-btn"]}
+        type="submit"
+        disabled={!email || !password}
+      >
         ورود
       </button>
 
