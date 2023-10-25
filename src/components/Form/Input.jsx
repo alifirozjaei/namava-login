970cc222ce06f44700f4009a524d0c0c51541441
Ali L Firozjaeai <ali.firozjaeii@gmail.com>
2023-10-26 00:39:21 +0330

    add show & hidden password feature for password input

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./input.module.css";
+import ShowEyeIcon from "../Icons/ShowEyeIcon.jsx";
+import { HideEyeIcon } from "../Icons/HideEyeIcon.jsx";
 const Input = ({ id, type, placeholder, label, changeHandler, value }) => {
+  const [isHiddenPasswrd, setIsHiddenPassword] = useState(true);
+
+  const getInputType = (isHidden) => {
+    if (isHidden) {
+      return "password";
+    } else {
+      return "text";
+    }
+  };
+
   return (
     <div className={styles["form-control"]}>
       <label htmlFor="passwrod" className={styles["form-label"]}>
         {label}
       </label>
       <input
-        type={type}
+        type={type == "password" ? getInputType(isHiddenPasswrd) : type}
         id={id}
         className={styles["form-input"]}
         placeholder={placeholder}
         onChange={(e) => changeHandler(e.target.value)}
         value={value}
       />
+      {type == "password" && !!value && (
+        <span
+          className={styles["hide-button"]}
+          onClick={() => setIsHiddenPassword((hp) => !hp)}
+        >
+          {isHiddenPasswrd && <ShowEyeIcon />}
+          {!isHiddenPasswrd && <HideEyeIcon />}
+        </span>
+      )}
     </div>
   );
 };
